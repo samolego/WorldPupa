5b77dbc99f0d248539084f5a48b5e203a3449fe8
samo_lego <34912839+samolego@users.noreply.github.com>
2020-04-21 15:13:09 +0200

    Update renaming.sh

--- renaming.sh
+++ renaming.sh
@@ -7,6 +7,14 @@ rm *sources*
 rm *dev*
 
 cd ../../
+
+# Moving older builds to the build/libs folder (from dev-builds branch)
+git checkout dev-builds *.jar
+mv *.jar ./build/libs
+
+# Cleaning up old dev builds, if there are more than 10
+ls ./build/libs -1tr | tail -n +10 | xargs rm -f
+
 # Setting the variables
 projectName="`grep '"id":' src/main/resources/fabric.mod.json | sed 's/"id"://g;s/ //g;s/,//g;s/"//g'`"
 version="`grep 'mod_version' gradle.properties | grep -o '[0-9]*\.[0-9]*\.[0-9]*'`"
@@ -14,6 +22,6 @@ mcVersion="`grep 'minecraft_version' gradle.properties | grep -o '[0-9]*\.[0-9]*
 
 cd ./build/libs
 
-echo "Build is going to be renamed: $projectName-$version-devbuild-$GITHUB_RUN_NUMBER-MC-$mcVersion.jar"
+echo "Build is going to be renamed: $projectName-$version-devbuild_$GITHUB_RUN_NUMBER-MC_$mcVersion.jar"
 # Renaming the dev build
-mv "$projectName-$version.jar" "$projectName-$version-devbuild-$GITHUB_RUN_NUMBER-MC-$mcVersion.jar"
+mv "$projectName-$version.jar" "$projectName-$version-devbuild_$GITHUB_RUN_NUMBER-MC_$mcVersion.jar"
