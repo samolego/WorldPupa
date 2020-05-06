922f23267d3aaae012fcc59d94d168cc19da30c5
samo_lego <34912839+samolego@users.noreply.github.com>
2020-05-06 11:41:29 +0200

    Spaces fix

--- renaming.sh
+++ renaming.sh
@@ -17,7 +17,8 @@ cd ./build/libs
 
 echo "Build is going to be renamed: $projectId-$version-devbuild_$GITHUB_RUN_NUMBER-MC_$mcVersion"
 # Renaming the dev build
-buildName = "$projectId-$version-devbuild_$GITHUB_RUN_NUMBER-MC_$mcVersion.jar"
+buildName="$projectId-$version-devbuild_$GITHUB_RUN_NUMBER-MC_$mcVersion.jar"
+
 mv "$projectId-$version.jar" "$buildName.jar"
 
 echo "::set-env name=BUILDNAME::$buildName"
