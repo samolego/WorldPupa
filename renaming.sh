97759fe53f4680f9c848ae80e7a9ca94b678a596
samo_lego <34912839+samolego@users.noreply.github.com>
2020-05-06 08:57:35 +0200

    Trying out artifacts

--- renaming.sh
+++ renaming.sh
@@ -8,20 +8,13 @@ rm *dev*
 
 cd ../../
 
-# Moving older builds to the build/libs folder (from dev-builds branch)
-git checkout origin/dev-builds *.jar
-mv *.jar ./build/libs
-
-# Cleaning up old dev builds, if there are more than 10
-ls ./build/libs -1tr | tail -n +10 | xargs rm -f
-
 # Setting the variables
-projectName="`grep '"id":' src/main/resources/fabric.mod.json | sed 's/"id"://g;s/ //g;s/,//g;s/"//g'`"
+projectId="`grep '"id":' src/main/resources/fabric.mod.json | sed 's/"id"://g;s/ //g;s/,//g;s/"//g'`"
 version="`grep 'mod_version' gradle.properties | grep -o '[0-9]*\.[0-9]*\.[0-9]*'`"
-mcVersion="`grep 'minecraft_version' gradle.properties | grep -o '[0-9]*\.[0-9]*\.[0-9]*'`"
+mcVersion="$(grep 'minecraft_version' gradle.properties | sed 's/minecraft_version=//g;s/ //g;s/,//g;s/"//g')"
 
 cd ./build/libs
 
-echo "Build is going to be renamed: $projectName-$version-devbuild_$GITHUB_RUN_NUMBER-MC_$mcVersion.jar"
+echo "Build is going to be renamed: $projectId-$version-devbuild_$GITHUB_RUN_NUMBER-MC_$mcVersion.jar"
 # Renaming the dev build
-mv "$projectName-$version.jar" "$projectName-$version-devbuild_$GITHUB_RUN_NUMBER-MC_$mcVersion.jar"
+mv "$projectId-$version.jar" "$projectId-$version-devbuild_$GITHUB_RUN_NUMBER-MC_$mcVersion.jar"
